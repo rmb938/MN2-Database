f08973aaabdfebf95e47ea527be9d323b726e782
Jodie Belgrave <rmb1993@gmail.com>
2014-04-01 22:37:17 -0400

    Allow api access

--- src/main/java/com/rmb938/database/DatabaseAPI.java
+++ src/main/java/com/rmb938/database/DatabaseAPI.java
@@ -1,4 +1,23 @@
 package com.rmb938.database;
 
 public class DatabaseAPI {
+
+    private static MongoDatabase mongoDatabase;
+    private static MySQLDatabase mySQLDatabase;
+
+    public static void initializeMongo(String database, String address, int port) {
+        mongoDatabase = new MongoDatabase(database, address, port);
+    }
+
+    public static void initializeMySQL(String userName, String password, String database, String address, int port) {
+        mySQLDatabase = new MySQLDatabase(userName, password, database, address, port);
+    }
+
+    public static MongoDatabase getMongoDatabase() {
+        return mongoDatabase;
+    }
+
+    public static MySQLDatabase getMySQLDatabase() {
+        return mySQLDatabase;
+    }
 }
